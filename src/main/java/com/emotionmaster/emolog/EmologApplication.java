bb0e02d33a80181f0165e7ab882ed57beec8fab2
X1n9fU <minkyeong0244@gmail.com>
2024-07-09 16:48:25 +0900

    [init] Spring 3.3.1 / Java 17 / Spring Web, Spring Data JPA, Lombok, MySQl Driver

---
+++ src/main/java/com/emotionmaster/emolog/EmologApplication.java
@@ -0,0 +1,13 @@
+package com.emotionmaster.emolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmologApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emotionmaster/emolog/EmologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emotionmaster.emolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
