5ff41b70ac8139c4a467c9a9d4713dddc5769b24
X1n9fU <minkyeong0244@gmail.com>
2024-07-14 21:20:12 +0900

    [fix] AddDiaryResponse dto 추가

--- src/main/java/com/emotionmaster/emolog/diary/controller/DiaryController.java
+++ src/main/java/com/emotionmaster/emolog/diary/controller/DiaryController.java
@@ -2,6 +2,7 @@ package com.emotionmaster.emolog.diary.controller;
 
 import com.emotionmaster.emolog.diary.domain.Diary;
 import com.emotionmaster.emolog.diary.dto.request.AddDiaryRequest;
+import com.emotionmaster.emolog.diary.dto.response.AddDiaryResponse;
 import com.emotionmaster.emolog.diary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,9 +20,9 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @PostMapping
-    public ResponseEntity<Diary> saveDiary(@RequestBody AddDiaryRequest request){
+    public ResponseEntity<AddDiaryResponse> saveDiary(@RequestBody AddDiaryRequest request){
         Diary savedDiary = diaryService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(savedDiary);
+                .body(new AddDiaryResponse(savedDiary));
     }
 }
