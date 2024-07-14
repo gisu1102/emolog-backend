e4479e27db7a3604301ac5cecf22bf666c000093
X1n9fU <minkyeong0244@gmail.com>
2024-07-14 20:48:42 +0900

    [feat] Diary (Diary, Emotion, Q_A 포함) 저장

---
+++ src/main/java/com/emotionmaster/emolog/diary/controller/DiaryController.java
@@ -0,0 +1,27 @@
+package com.emotionmaster.emolog.diary.controller;
+
+import com.emotionmaster.emolog.diary.domain.Diary;
+import com.emotionmaster.emolog.diary.dto.request.AddDiaryRequest;
+import com.emotionmaster.emolog.diary.service.DiaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/diary")
+@RequiredArgsConstructor
+public class DiaryController {
+
+    private final DiaryService diaryService;
+
+    @PostMapping
+    public ResponseEntity<Diary> saveDiary(@RequestBody AddDiaryRequest request){
+        Diary savedDiary = diaryService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedDiary);
+    }
+}
