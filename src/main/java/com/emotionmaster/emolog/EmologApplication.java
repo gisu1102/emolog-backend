adbd7015cd935b7fd74e3995d8f746c4fb6ec6a3
X1n9fU <minkyeong0244@gmail.com>
2024-07-14 20:44:38 +0900

    [feat] BaseEntity를 위한 @EnableJpaAuditing 추가

--- src/main/java/com/emotionmaster/emolog/EmologApplication.java
+++ src/main/java/com/emotionmaster/emolog/EmologApplication.java
@@ -2,8 +2,10 @@ package com.emotionmaster.emolog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EmologApplication {
 
 	public static void main(String[] args) {
