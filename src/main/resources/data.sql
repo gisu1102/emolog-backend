d9bc3367f3897f2e933725e5baf7e1d1eac6e29d
X1n9fU <minkyeong0244@gmail.com>
2024-07-15 19:18:38 +0900

    [chore] h2 database 및 data.sql 데이터 셋 구축 #8

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO diary(id, diary_date, content) VALUES (1,'2024-07-15', 'content');
+INSERT INTO emotion(emotion, diary_id) VALUES ('emotion', 1);
+INSERT INTO color(red, green, blue, hexa, diary_id) VALUES (0, 0, 0, 'FFFFFF', 1);
+INSERT INTO q_a(question, answer, diary_id) VALUES ('question', 'answer', 1);
+
+
+INSERT INTO diary(id, diary_date, content) VALUES (2, '2024-07-16', 'content');
+INSERT INTO color(red, green, blue, hexa, diary_id) VALUES (0, 0, 0, 'FFFFFF', 2);
+
+INSERT INTO diary(id, diary_date, content) VALUES (3, '2024-07-17', 'content');
+INSERT INTO color(red, green, blue, hexa, diary_id) VALUES (0, 0, 0, 'FFFFFF', 3);
