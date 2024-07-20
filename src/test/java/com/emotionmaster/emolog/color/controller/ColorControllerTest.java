fbb13fcc42c26b72906b9f57f20298ad6d0a1d3f
X1n9fU <minkyeong0244@gmail.com>
2024-07-20 22:30:29 +0900

    [test] findAllColorOfMonthAndWeek() 테스트

--- src/test/java/com/emotionmaster/emolog/color/controller/ColorControllerTest.java
+++ src/test/java/com/emotionmaster/emolog/color/controller/ColorControllerTest.java
@@ -4,6 +4,7 @@ import com.emotionmaster.emolog.color.domain.Color;
 import com.emotionmaster.emolog.color.repository.ColorRepository;
 import com.emotionmaster.emolog.diary.domain.Diary;
 import com.emotionmaster.emolog.diary.repository.DiaryRepository;
+import com.emotionmaster.emolog.util.DateUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -47,21 +48,24 @@ class ColorControllerTest {
         diaryRepository.deleteAll();
     }
 
-    @DisplayName("findAllColorOfMonth() : month 값에 맞춰 월간 색을 모두 조회한다")
     @Test
+    @DisplayName("findAllColorOfMonth() : month 값에 맞춰 월간 색을 모두 조회한다")
     public void findAllColorOfMonth() throws Exception {
         final String url = "/api/color";
+
         final String content1 = "content1";
         final String content7 = "content7";
+
         final String color1 = "FFFFFF";
         final String color7 = "000000";
-        final LocalDate date1 = LocalDate.of(2024,1,20);
-        final LocalDate date7 = LocalDate.now();
 
-        Diary diaryOf7 = diaryRepository.save(new Diary(date7, content7));
+        final LocalDate date1 = LocalDate.of(2024, 1, 20);
+        final LocalDate date7 = LocalDate.of(2024, 7, 20);
+
+        Diary diaryOf7 = diaryRepository.save(new Diary(date7, content7, DateUtil.getWeekOfMonthByDate(date7)));
         colorRepository.save(new Color(0, 0, 0, color7, diaryOf7));
 
-        Diary diaryOf1 = diaryRepository.save(new Diary(date1, content1));
+        Diary diaryOf1 = diaryRepository.save(new Diary(date1, content1, DateUtil.getWeekOfMonthByDate(date1)));
         colorRepository.save(new Color(0, 0, 0, color1, diaryOf1));
 
         // Test for July (month 7)
@@ -69,13 +73,54 @@ class ColorControllerTest {
                         .param("month", String.valueOf(7)))
                 .andExpect(status().isOk());
 
-        resultsOf7.andExpect(jsonPath("$[0].['" + date7 + "']").value(color7));
+        resultsOf7.andExpect(jsonPath("$[0].date").value(String.valueOf(date7)))
+                .andExpect(jsonPath("$[0].hexa").value(color7));
 
         // Test for January (month 1)
         ResultActions resultsOf1 = mockMvc.perform(get(url)
                         .param("month", String.valueOf(1)))
                 .andExpect(status().isOk());
 
-        resultsOf1.andExpect(jsonPath("$[0].['" + date1 + "']").value(color1));
+        resultsOf1.andExpect(jsonPath("$[0].date").value(String.valueOf(date1)))
+                .andExpect(jsonPath("$[0].hexa").value(color1));
+    }
+
+    @Test
+    @DisplayName("findAllColorOfMonthAndWeek() : week 값에 맞춰 월간 색을 모두 조회한다")
+    public void findAllColorOfMonthAndWeek() throws Exception {
+        final String url = "/api/color";
+
+        final String content1 = "content1";
+        final String content7 = "content7";
+
+        final String color1 = "FFFFFF";
+        final String color7 = "000000";
+
+        final LocalDate dateOfWeek4 = LocalDate.of(2024, 7, 21);
+        final LocalDate dateOfWeek3 = LocalDate.of(2024, 7, 20);
+
+        Diary diaryOf7 = diaryRepository.save(new Diary(dateOfWeek3, content7, DateUtil.getWeekOfMonthByDate(dateOfWeek3)));
+        colorRepository.save(new Color(0, 0, 0, color7, diaryOf7));
+
+        Diary diaryOf1 = diaryRepository.save(new Diary(dateOfWeek4, content1, DateUtil.getWeekOfMonthByDate(dateOfWeek4)));
+        colorRepository.save(new Color(0, 0, 0, color1, diaryOf1));
+
+        // Test for July (month 7)
+        ResultActions resultsOf7 = mockMvc.perform(get(url)
+                        .param("month", String.valueOf(7))
+                        .param("week", String.valueOf(3)))
+                .andExpect(status().isOk());
+
+        resultsOf7.andExpect(jsonPath("$[0].date").value(String.valueOf(dateOfWeek3)))
+                .andExpect(jsonPath("$[0].hexa").value(color7));
+
+        // Test for January (month 1)
+        ResultActions resultsOf1 = mockMvc.perform(get(url)
+                        .param("month", String.valueOf(7))
+                        .param("week", String.valueOf(4)))
+                .andExpect(status().isOk());
+
+        resultsOf1.andExpect(jsonPath("$[0].date").value(String.valueOf(dateOfWeek4)))
+                .andExpect(jsonPath("$[0].hexa").value(color1));
     }
 }
