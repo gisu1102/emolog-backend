81fcf80dc2fba29d109a4c00a39560afc4fd015a
X1n9fU <minkyeong0244@gmail.com>
2024-07-21 15:55:47 +0900

    [fix] DayOfWeek 요일 추가

--- src/main/java/com/emotionmaster/emolog/color/dto/response/ColorAndDate.java
+++ src/main/java/com/emotionmaster/emolog/color/dto/response/ColorAndDate.java
@@ -1,20 +1,26 @@
 package com.emotionmaster.emolog.color.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
+// 월간, 주간의 색을 날짜와 같이 반환해주는 DTO
 public class ColorAndDate {
     private LocalDate date;
     private String hexa;
+    private DayOfWeek dayOfWeek;
 
-    public ColorAndDate(LocalDate date, String hexa) {
+    @Builder
+    public ColorAndDate(LocalDate date, String hexa, DayOfWeek dayOfWeek) {
         this.date = date;
         this.hexa = hexa;
+        this.dayOfWeek = dayOfWeek;
     }
 }
