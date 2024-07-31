b39e4e237b701348565a62a88e36d0061f85167b
X1n9fU <minkyeong0244@gmail.com>
2024-07-31 16:21:26 +0900

    [feat] StrToDate 생성

--- src/main/java/com/emotionmaster/emolog/util/DateUtil.java
+++ src/main/java/com/emotionmaster/emolog/util/DateUtil.java
@@ -1,6 +1,11 @@
 package com.emotionmaster.emolog.util;
 
+import com.emotionmaster.emolog.config.error.errorcode.DiaryErrorCode;
+import com.emotionmaster.emolog.config.error.exception.DiaryException;
+import com.emotionmaster.emolog.diary.domain.Diary;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.WeekFields;
 import java.util.Locale;
 
@@ -8,5 +13,10 @@ public class DateUtil {
     public static int getWeekOfMonthByDate(LocalDate date) {
         WeekFields weekFields = WeekFields.of(Locale.UK); //월요일 부터 시작
         return date.get(weekFields.weekOfMonth());
+    }
+
+    public static LocalDate strToDate(String date){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return LocalDate.parse(date,dtf);
     }
 }
